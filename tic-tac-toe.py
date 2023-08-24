fb82b84679a5835434e7a6495991913031939afb
ChaitanyaMane <141495750+ChaitanyaMane@users.noreply.github.com>
2023-08-24 17:56:38 +0530

    change

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,43 +1,43 @@
-import random
 import tkinter as tk
-from tkinter import messagebox
+import random
 
-class TicTacToeGUI:
+
+class TicTacToe:
     def __init__(self, root):
         self.root = root
         self.root.title("Tic Tac Toe")
         self.board = [[" " for _ in range(3)] for _ in range(3)]
-        self.players = ["X", "O"]
-        self.current_turn = 0
+        self.current_player = "X"
 
-        self.buttons = []
+        self.buttons = [[None]*3 for _ in range(3)]
         for row in range(3):
-            button_row = []
             for col in range(3):
-                button = tk.Button(root, text=" ", font=("Helvetica", 24), height=2, width=5,
-                                   command=lambda row=row, col=col: self.on_button_click(row, col))
-                button.grid(row=row, column=col)
-                button_row.append(button)
-            self.buttons.append(button_row)
+                self.buttons[row][col] = tk.Button(root, text=" ", font=("Helvetica", 24), height=2, width=5,
+                                                   command=lambda r=row, c=col: self.make_move(r, c))
+                self.buttons[row][col].grid(row=row, column=col)
 
-    def on_button_click(self, row, col):
-        if self.board[row][col] == " " and self.players[self.current_turn] == "X":
-            self.board[row][col] = "X"
-            self.buttons[row][col].config(text="X", state=tk.DISABLED)
-            self.current_turn = 1 - self.current_turn
-            self.check_game_status()
+    def make_move(self, row, col):
+        if self.board[row][col] == " ":
+            self.board[row][col] = self.current_player
+            self.buttons[row][col].config(text=self.current_player)
 
-            if not self.check_winner("X") and not self.is_board_full():
-                self.computer_move()
+            if self.check_winner(self.current_player):
+                self.display_winner(self.current_player)
+            elif self.is_board_full():
+                self.display_draw()
+            else:
+                self.current_player = "O" if self.current_player == "X" else "X"
+                if self.current_player == "O":
+                    self.computer_move()
 
     def computer_move(self):
         empty_cells = self.get_empty_cells()
         if empty_cells:
             row, col = random.choice(empty_cells)
-            self.board[row][col] = "O"
-            self.buttons[row][col].config(text="O", state=tk.DISABLED)
-            self.current_turn = 1 - self.current_turn
-            self.check_game_status()
+            self.make_move(row, col)
+
+    def get_empty_cells(self):
+        return [(row, col) for row in range(3) for col in range(3) if self.board[row][col] == " "]
 
     def check_winner(self, player):
         for row in self.board:
@@ -46,45 +46,53 @@ class TicTacToeGUI:
         for col in range(3):
             if all([self.board[row][col] == player for row in range(3)]):
                 return True
-        if all([self.board[i][i] == player for i in range(3)]) or all([self.board[i][2-i] == player for i in range(3)]):
+        if all([self.board[i][i] == player for i in range(3)]) or all([self.board[i][2 - i] == player for i in range(3)]):
             return True
         return False
 
     def is_board_full(self):
         return all([cell != " " for row in self.board for cell in row])
 
-    def get_empty_cells(self):
-        empty_cells = []
-        for row in range(3):
-            for col in range(3):
-                if self.board[row][col] == " ":
-                    empty_cells.append((row, col))
-        return empty_cells
+    def display_winner(self, player):
+        winner_label = tk.Label(
+            self.root, text=f"{player} wins!", font=("Helvetica", 16))
+        winner_label.grid(row=3, columnspan=3)
+        self.disable_buttons()
+        self.add_restart_button()
 
-    def check_game_status(self):
-        if self.check_winner("X"):
-            self.show_winner_message("X")
-        elif self.check_winner("O"):
-            self.show_winner_message("O")
-        elif self.is_board_full():
-            self.show_draw_message()
+    def display_draw(self):
+        draw_label = tk.Label(
+            self.root, text="It's a draw!", font=("Helvetica", 16))
+        draw_label.grid(row=3, columnspan=3)
+        self.disable_buttons()
+        self.add_restart_button()
 
-    def show_winner_message(self, player):
-        messagebox.showinfo("Game Over", f"Player {player} wins!")
-        self.reset_board()
-
-    def show_draw_message(self):
-        messagebox.showinfo("Game Over", "It's a draw!")
-        self.reset_board()
-
-    def reset_board(self):
+    def disable_buttons(self):
         for row in self.buttons:
             for button in row:
-                button.config(text=" ", state=tk.NORMAL)
-        self.board = [[" " for _ in range(3)] for _ in range(3)]
-        self.current_turn = 0
+                button.config(state="disabled")
+
+    def add_restart_button(self):
+        restart_button = tk.Button(self.root, text="Restart", font=(
+            "Helvetica", 14), command=self.restart_game)
+        restart_button.grid(row=4, columnspan=3)
+
+    def restart_game(self):
+        for row in range(3):
+            for col in range(3):
+                self.board[row][col] = " "
+                self.buttons[row][col].config(text=" ", state="normal")
+        self.current_player = "X"
+        self.clear_labels()
+
+    def clear_labels(self):
+        for child in self.root.winfo_children():
+            if isinstance(child, tk.Label):
+                child.destroy()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
-    game = TicTacToeGUI(root)
+    game = TicTacToe(root)
     root.mainloop()
+    
